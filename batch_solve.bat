e6a523c7eff9c800d6ef8fc94a0868bc6b5324fc
Paolo Lammens <lammenspaolo@gmail.com>
2018-11-14 22:15:33 +0100

    fix: batch_solve

--- batch_solve.bat
+++ batch_solve.bat
@@ -1,7 +1,7 @@
-del test.txt
+del batch_solve.txt
 
-for /L %%n in (1,1,4) do python solve.py 41 %%n bland >> batch_solve.txt & python solve.py 41 %%n no_bland >> batch_solve.txt
+for /L %%n in (1,1,4) do python solve.py 41 %%n --rule bland >> batch_solve.txt & python solve.py 41 %%n --rule minrc >> batch_solve.txt
 
-for /L %%n in (1,1,4) do python solve.py 70 %%n bland >> batch_solve.txt & python solve.py 70 %%n no_bland >> batch_solve.txt
+for /L %%n in (1,1,4) do python solve.py 70 %%n --rule bland >> batch_solve.txt & python solve.py 70 %%n --rule minrc >> batch_solve.txt
 
 PAUSE
