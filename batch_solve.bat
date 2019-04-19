978d8b787ea0429594b472e44baa93febcc3abad
Paolo Lammens <lammenspaolo@gmail.com>
2019-04-19 18:08:40 +0200

    Update batch_solve.bat

--- batch_solve.bat
+++ batch_solve.bat
@@ -1,7 +1,16 @@
-del batch_solve.txt
+@comment
+This batch file tests the main program with some sample
+test cases.
+@endcomment
 
-for /L %%n in (1,1,4) do python solve.py 41 %%n --rule bland >> batch_solve.txt & python solve.py 41 %%n --rule minrc >> batch_solve.txt
+@del batch_solve.txt
 
-for /L %%n in (1,1,4) do python solve.py 70 %%n --rule bland >> batch_solve.txt & python solve.py 70 %%n --rule minrc >> batch_solve.txt
+@for /L %%n in (1,1,4) do ^
+    python solve.py 41 %%n --rule bland >> batch_solve.txt & ^
+    python solve.py 41 %%n --rule minrc >> batch_solve.txt
 
-PAUSE
+@for /L %%n in (1,1,4) do ^
+    python solve.py 70 %%n --rule bland >> batch_solve.txt & ^
+    python solve.py 70 %%n --rule minrc >> batch_solve.txt
+
+@pause
